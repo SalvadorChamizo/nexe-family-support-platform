1f790cac8d85bf24d662d444cf8f183c4a550f85
Salvador Chamizo <salvadorchamizo@gmail.com>
2025-11-22 17:49:37 +0100

    feat: Add auth user state

---
+++ frontend/src/state/authState.ts
@@ -0,0 +1,66 @@
+let accessToken: string | null = null;
+
+const apiHost = `${window.location.hostname}`;
+
+export let tempToken = "";
+export let tempUserId = 0;
+export let tempUsername = "";
+
+export function setTemp2FA(token: string, username: string, userId: number) {
+    tempToken = token;
+    tempUsername = username;
+    tempUserId = userId;
+}
+
+export function setAccessToken(token: string) {
+    accessToken = token;
+}
+
+export function getAccessToken(): string | null {
+    return (accessToken);
+}
+
+export function isLoggedIn(): boolean {
+    return (accessToken !== null);
+}
+
+export function clearAuth() {
+    accessToken = null;
+}
+
+export function getUserIdFromToken(): number | null {
+    if (!accessToken) return null;
+    
+    try {
+        const payload = JSON.parse(atob(accessToken.split('.')[1]));
+        return payload.id || null;
+    } catch (error) {
+        return null;
+    }
+}
+
+export async function refreshAccessToken(): Promise<boolean> {
+
+    try {
+        const res = await fetch(`http://${apiHost}:8080/auth/refresh`, {
+            method: "POST",
+            credentials: "include",
+        });
+    
+        const data = await res.json();
+        if (!res.ok || data.success === false) {
+            clearAuth();
+            return (false);
+        }
+
+        if (data.accessToken) {
+            setAccessToken(data.accessToken);
+            return (true);
+        }
+        clearAuth();
+        return (false);
+    } catch (err) {
+        clearAuth();
+        return (false);
+    }
+}
