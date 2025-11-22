3e82d1d88595233e9688e370f1bea13e0840b835
Salvador Chamizo <salvadorchamizo@gmail.com>
2025-11-22 14:13:57 +0100

    feat: Add basic routes for testing

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,7 +1,14 @@
+import { Home } from "./pages/Home/homeTemplate";
+import { Login } from "./pages/Login/loginTemplate";
 
 
-export async function router(route: string): string {
+export function router(route: string): string {
     switch (route) {
-        
+        case "/login":
+            return Login();
+        case "":
+            return Home();
+        default:
+            return Home();
     }
 }
