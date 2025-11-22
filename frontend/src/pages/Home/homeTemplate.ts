fa5cd8ad15e31a6f0646ee235889009c90d39d4f
Salvador Chamizo <salvadorchamizo@gmail.com>
2025-11-22 14:14:31 +0100

    feat: Add basic pages for testing

---
+++ frontend/src/pages/Home/homeTemplate.ts
@@ -0,0 +1,10 @@
+export function Home(): string {
+
+    return `
+    <p>Nexe Fundació somos una entidad que trabajamos desde hace 40 años con la voluntad de mejorar la calidad de vida de los niños con pluridiscapacidad y sus familias y defender sus derechos.
+
+A lo largo de nuestra trayectoria, hemos creado una red de servicios innovadores y pioneros – reconocidos en el ámbito estatal y europeo – para dar respuesta a las necesidades de los niños y sus familias, favoreciendo su autonomía y socialización.
+
+Así mismo, compartimos nuestra experiencia formando otros profesionales, desarrollando proyectos de investigación y sensibilizando a la sociedad.</p>
+    `;
+}
---
+++ frontend/src/pages/Login/loginTemplate.ts
@@ -0,0 +1,5 @@
+export function Login(): string {
+    return `
+        <h1>Login Page</h1>
+    `;
+}
