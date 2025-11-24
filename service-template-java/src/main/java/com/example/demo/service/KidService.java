f524f7c76e7b3374721fc702927675e417edc64b
marcos <marcoszabalarodriguez@gmail.com>
2025-11-24 10:06:08 +0100

    Feature: Added KidService

--- service-template-java/src/main/java/com/example/demo/service/KidService.java
+++ service-template-java/src/main/java/com/example/demo/service/KidService.java
@@ -2,21 +2,32 @@ package com.example.demo.service;
 
 import com.example.demo.entity.Kids;
 import com.example.demo.repository.KidRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
-@RestController
-@RequestMapping("/kids")
+@Service
 public class KidService {
-    private KidService service;
+    private final KidRepository repository;
 
-    public void KidRepository(KidService service) {
-        this.service = service;
+    public KidService(KidRepository repository) {
+        this.repository = repository;
     }
 
-    public List<Kids> getAllKids() {
-        return .findAll();
+    public List<Kids> findAll() {
+        return repository.findAll();
+    }
+
+    public Optional<Kids> findByID(Long id) {
+        return repository.findById(id);
+    }
+
+    public Kids save(Kids kid) {
+        return repository.save(kid);
+    }
+
+    public void deleteById(Long id) {
+        repository.deleteById(id);
     }
 }
